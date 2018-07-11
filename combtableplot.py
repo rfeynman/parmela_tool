cd66531fc06ea8d2494428d4016a3a67766eb154
rfeynman <wedwrd@gmail.com>
2018-07-11 15:44:59 -0400

    combtable bug fix

--- combtableplot.py
+++ combtableplot.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
 '''
 Created on Jul 19, 2017
 
 @author: wange
 combine multiple colume into sigle TIMESTEP file. choose the coleum and phase steps and run the code
+THE usefuldata number is the line with "Data"
 '''
 
 import numpy as np
@@ -12,57 +12,57 @@ import string
 
 
 
-fold = "test"
-term = "TIMESTEPEMITTANCE"
+fold = "shielding"
+term = "GUN35002_"
 suf = ".TBL"
-begin_number=90
-end_number=132
-intvel=3
-listrange = list(range(90, end_number+1, intvel)) # generate the number list of the TIMESTEPEMITT???
-print(listrange)
-
+listrange = [0,1.3,1.4,1.9,2.1,11.9,41.9,441.9]
+dataline=39
 def linepick(pick):
     sumdata=[]
     datanum=[]
-    for intr in listrange[1:]:#iteration for open multiple TIMESTEP file
+    for intr in listrange[1:]:
         with open("../" + fold + "/" + term + str(intr) + suf) as f:
             l = [line.split() for line in f]
         f.close()
-        usefuldata = l[84:] #84 is starting the table header
+        usefuldata = l[dataline:]
 
         # content=genfromtxt("../"+fold+"/"+term+"90"+suf,delimiter='/t',names=True,dtype=None)
         headname = [elem for elem in usefuldata[0]][1:]
         usefuldata[0]=headname
-        col_usefuldata={usefuldata[0][i]:[line[i] for line in usefuldata] for i in range(len(usefuldata[0]))} # generate the row as dictionary and rotate{header:head xx xx... xx}
+        col_usefuldata={usefuldata[0][i]:[line[i] for line in usefuldata] for i in range(len(usefuldata[0]))}
         for i in range(len(headname)):
-            col_usefuldata[headname[i]][0]=headname[i]+str(intr) # add number on the header
+            col_usefuldata[headname[i]][0]=headname[i]+str(intr)
             
         for elem in pick:
-            sumdata.append(col_usefuldata[headname[elem]]) #generate a table with multiple lines 
+            sumdata.append(col_usefuldata[headname[elem]])
         #print(col_usefuldata[headname[0]])
-        datanum.append(len(usefuldata)) #check the multiple files length, only minimum list will be applied
+        #print[col_usefuldata]
+        datanum.append(len(usefuldata))
+	
     minnum=min(datanum)
-    with open("../" + fold + "/" + term + str(listrange[0]) + suf) as f0: #open first file
+    with open("../" + fold + "/" + term + str(listrange[0]) + suf) as f0:
         l0=f0.readlines()
     #print(l0[84])
     f0.close()
     
     #print(l0[84].strip('\n'),str(sumdata[0][0]),"\n",'    '.join([l0[84].strip('\n'),str(sumdata[0][0])]))
 
-    
-    for k in range(84,minnum):
+    print[sumdata[1][0]]
+    for k in range(0,minnum):
+
         for i in range(len(sumdata)):
             
-            l0[k]='    '.join([l0[k].strip('\n'),str(sumdata[i][k-84])+'\n']) # append the new table to the end
-            
-    inslabel=l0.index('Ezref(MV/m)\n')
+            l0[k+dataline]='    '.join([l0[k+dataline].strip('\r\n'),str(sumdata[i][k])+'\r\n'])
     
-    del l0[(minnum):]
+    del l0[minnum+dataline:]
+   
+    inslabel=l0.index('V \r\n')
+	
 
-    for i in range(len(sumdata)): #insert the title and label
-        l0.insert(inslabel+i+1,str(sumdata[i][0]+'\n'))
-        l0.insert(inslabel+2*i+25,str(sumdata[i][0]+'\n'))
-        l0.insert(inslabel+3*i+49,str(sumdata[i][0]+'\n'))
+    for i in range(len(sumdata)):
+        l0.insert(inslabel+i+1,str(sumdata[i][0]+'\r\n'))
+        l0.insert(inslabel+2*i+11,str(sumdata[i][0]+'\r\n'))
+        l0.insert(inslabel+3*i+21,str(sumdata[i][0]+'\r\n'))
     outputfile=open("../" + fold + "/" + term + "sum" + suf,'w')
     outputfile.writelines(l0)
     outputfile.close()
@@ -71,12 +71,7 @@ def linepick(pick):
     
 
 def main():
-    '''
-    T(deg):0         Z(cm):1        Xun(mm-mrad):2   Yun(mm-mrad):3   Zun(mm-mrad):4  Xn(mm-mrad):5    Yn(mm-mrad):6    
-    Zn(mm-mrad):7     Xrms(mm):8       Yrms(mm):9      Zrmz(mm):10       <kE>(MeV):11           Del-Erms:12       <X>(mm):13      
-    <Xpn>(mrad):14   <Y>(mm):15        <Ypn>(mrad):16  <Z>(cm):17       <Zpn>(rad):18   EZref(MV/m):19
-    '''
-    interestcol=[3,5,6,8]
+    interestcol=[2,3,4]
     
     linepick(interestcol)
     
